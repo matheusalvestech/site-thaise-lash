9398d0ea882f6adb61446deace09794287c7564e
matheusalvestech <alves84195@gmail.com>
2026-06-20 22:55:07 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const mobileMenu = document.getElementById('mobile-menu');
+const navList = document.querySelector('.nav-list');
 
-        const navList = document.querySelector('.nav-list');
-        mobileMenu.addEventListener('click', () => {
-            mobileMenu.classList.toggle('is-active');
-            navList.classList.toggle('active');
-        });
+mobileMenu.addEventListener('click', () => {
+    mobileMenu.classList.toggle('is-active');
+    navList.classList.toggle('active');
+});
 
-        document.querySelectorAll('.nav-list li a').forEach(link => {
-            link.addEventListener('click', () => {
-                mobileMenu.classList.remove('is-active');
-                navList.classList.remove('active');
-            });
-        });
+document.querySelectorAll('.nav-list li a').forEach(link => {
+    link.addEventListener('click', () => {
+        mobileMenu.classList.remove('is-active');
+        navList.classList.remove('active');
+    });
+});
