671455527ff3b42514b4e00fbcccd09b8fc47caf
matheusalvestech <alves84195@gmail.com>
2026-06-20 21:52:45 -0300

    Primeiro commit - Site finalizado Thaise

---
+++ script.js
@@ -0,0 +1,14 @@
+const mobileMenu = document.getElementById('mobile-menu');
+
+        const navList = document.querySelector('.nav-list');
+        mobileMenu.addEventListener('click', () => {
+            mobileMenu.classList.toggle('is-active');
+            navList.classList.toggle('active');
+        });
+
+        document.querySelectorAll('.nav-list li a').forEach(link => {
+            link.addEventListener('click', () => {
+                mobileMenu.classList.remove('is-active');
+                navList.classList.remove('active');
+            });
+        });
